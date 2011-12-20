5fba96a1bb76b59367436bc36e5cf7b6b48d75b0
deivid <deivid@ubuntu.(none)>
2011-12-19 23:42:19 -0300

    pequenos ajustes

--- db/migrate/20111215170735_remove_funcionario_to_mesa.rb
+++
@@ -1,9 +0,0 @@
-class RemoveFuncionarioToMesa < ActiveRecord::Migration
-  def up
-    remove_column :mesas, :id_funcionario
-  end
-
-  def down
-    add_column :mesas, :id_funcionario, :integer
-  end
-end
